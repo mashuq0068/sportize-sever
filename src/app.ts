c86318cede194f733dac3e4eb229f31214710002
mashuq0068 <mashuq0068@gmail.com>
2024-07-10 12:34:36 +0600

    added product services

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
-import express, { Application } from "express";
-import cors from "cors";
+import express, { Application } from 'express';
+import cors from 'cors';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/not-found';
 const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/' , (req, res) => {
-    res.send("Sportize is running on server.")
-})
+app.get('/', (req, res) => {
+  res.send('Sportize is running on server.');
+});
+// global error
+app.use(globalErrorHandler);
+// not found error
+app.use(notFound);
 
-export default app
+export default app;
